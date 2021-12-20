c01ac276f2b1a3c7085a8639d443bed18669eaf6
marcoarrudajf <marcoarruda.dev@gmail.com>
2021-12-20 11:16:40 -0300

    Alguns ajustes de indentação

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,58 +1,66 @@
+//Esta função coloca o numero no visor
 function calcNum(numero) {
-  if (typeof inputVisor == "undefined") {
-    document.calcform.visor.value = "";
-  }
-  document.calcform.visor.value = document.calcform.visor.value + numero;
-  inputVisor = true;
-  console.log(inputVisor);
-}
-function calcLimpo(){
-     document.calcform.visor.value = '';
+    if (typeof inputVisor == "undefined") {
+        document.calcform.visor.value = "";
     }
-    function calcLigar(on){
-        document.calcform.visor.value = on;     
+    document.calcform.visor.value = document.calcform.visor.value + numero;
+    inputVisor = true;
 }
+
+// este qua clicado o botão AC limpa a calculadora
+function calcLimpo() {
+    document.calcform.visor.value = '';
+}
+
+// Liga a calculadora colocando o valor como zero no visor
+function calcLigar(on) {
+    document.calcform.visor.value = on;
+}
+
+// memoriza os valores no visor, esta função ainda precisa ser  mais elaborada
 function calcMemo(
-    memo){
-        if(memo == 'memomais'){
-            document.calcform.visor.value = 'M+cd . ';
-        }else if(memo == 'memomenos'){
-            document.calcform.visor.value = 'M- ';
+    memo) {
+    if (memo == 'memomais') {
+        document.calcform.visor.value = 'M+cd . ';
+    } else if (memo == 'memomenos') {
+        document.calcform.visor.value = 'M- ';
     }
 }
 
+//Faz as operações que forem pedidas pelo usuário
 function calcOper(oper, valor1, valor2) {
-  if (oper == "somar") {
-    var valor = parseFloat(valor1) + parseFloat(valor2);
-  } else if (oper == "subtrair") {
-    valor = valor1 - valor2;
-  } else if (oper == "multiplicar") {
-    valor = valor1 * valor2;
-  } else {
-    valor = valor1 / valor2;
-  }
-  return valor;
+    if (oper == "somar") {
+        var valor = parseFloat(valor1) + parseFloat(valor2);
+    } else if (oper == "subtrair") {
+        valor = valor1 - valor2;
+    } else if (oper == "multiplicar") {
+        valor = valor1 * valor2;
+    } else {
+        valor = valor1 / valor2;
+    }
+    return valor;
 }
 
-function calcValor(oper){
-   var valor = document.calcform.visor.value;
-   delete inputVisor;
+//Pega as operações e retorna seus respectivos valores e resultados no 
+//visor verificando todas a s tarfeas executadas
+function calcValor(oper) {
+    var valor = document.calcform.visor.value;
+    delete inputVisor;
 
-   if (typeof inputOper != 'undefined' && oper == 'resultado') {
-      inputValor = calcOper(inputOper, inputValor, valor);
-      document.calcform.visor.value = inputValor;
-      delete oper;
-      delete inputValor;
-      return(0);
-   }
+    if (typeof inputOper != 'undefined' && oper == 'resultado') {
+        inputValor = calcOper(inputOper, inputValor, valor);
+        document.calcform.visor.value = inputValor;
+        delete oper;
+        delete inputValor;
+        return (0);
+    }
 
-   if (typeof inputValor != 'undefined') {
-      inputValor = calcOper(inputOper, inputValor, valor);
-      inputOper = oper;
-      document.calcform.visor.value = inputValor;
-   } else {
-      inputValor = valor;
-      inputOper = oper;
-   }
-
+    if (typeof inputValor != 'undefined') {
+        inputValor = calcOper(inputOper, inputValor, valor);
+        inputOper = oper;
+        document.calcform.visor.value = inputValor;
+    } else {
+        inputValor = valor;
+        inputOper = oper;
+    }
 }
